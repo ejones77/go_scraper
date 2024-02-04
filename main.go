aa0a9544c41b57f70fab50c3adb5d4ecdb8aeb7e
ejones77 <e77jones@gmail.com>
2024-02-04 12:29:42 -0600

    structuring the article, paragraphs, and links

--- main.go
+++ main.go
@@ -2,23 +2,81 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/gocolly/colly/v2"
 )
 
-func main() {
+type Link struct {
+	Text string `json:"text"`
+	URL  string `json:"url"`
+}
 
-	c := colly.NewCollector()
-	url := "https://en.wikipedia.org/wiki/Robotics"
+type Paragraph struct {
+	Text  string `json:"text"`
+	Links []Link `json:"links"`
+}
 
-	c.OnHTML(".mw-parser-output p", func(e *colly.HTMLElement) {
-		text := e.Text
-		fmt.Println(text)
+type Article struct {
+	Title      string      `json:"text"`
+	Paragraphs []Paragraph `json:"paragraphs"`
+}
+
+/*
+type Section struct {
+	Title string `json:"title"`
+	Paragraphs []Paragraph `json:"paragraphs"`
+}
+
+type Article struct {
+	Title        string      `json:"title"`
+	Introduction []Paragraph   `json:"introduction"`
+	Sections     []Section  `json:"sections"`
+}
+*/
+
+func getLinks(e *colly.HTMLElement) []Link {
+	var links []Link
+	e.ForEach("a", func(_ int, e *colly.HTMLElement) {
+		link := Link{
+			Text: e.Text,
+			URL:  e.Attr("href"),
+		}
+		links = append(links, link)
 	})
+	return links
+}
 
-	err := c.Visit(url)
-	if err != nil {
-		log.Fatal(err)
-	}
+func getParagraphs(e *colly.HTMLElement) []Paragraph {
+	var paragraphs []Paragraph
+	e.ForEach("p", func(_ int, e *colly.HTMLElement) {
+		paragraph := Paragraph{
+			Text:  e.Text,
+			Links: getLinks(e),
+		}
+		paragraphs = append(paragraphs, paragraph)
+	})
+	return paragraphs
+}
+
+func getArticle(c *colly.Collector, url string) Article {
+	var article Article
+	c.OnHTML(".mw-content-container", func(e *colly.HTMLElement) {
+		article = Article{
+			Title:      e.ChildText("#firstHeading"),
+			Paragraphs: getParagraphs(e),
+		}
+	})
+	c.Visit(url)
+	return article
+}
+
+func main() {
+	url := "https://en.wikipedia.org/wiki/Robotics"
+	c := colly.NewCollector()
+	article := getArticle(c, url)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+	fmt.Printf("%+v\n", article)
 }
